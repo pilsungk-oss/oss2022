08c7ae4c065ae9ce97e1f1ec7421cbddd98e821f
Pilsung Kang(pilsungk-oss) <pilsungk+oss@gmail.com>
2022-03-30 16:27:47 +0900

    Add Hello.java

---
+++ Hello.java
@@ -0,0 +1,15 @@
+
+public class Hello {
+	public static void main(String args[]) {
+				
+		AddOne ao = new AddOne();
+		int y = ao.addOne(10);
+		
+		System.out.println("y = " + y);
+				
+		/*
+		for (int i=0; i<xx.length; i++)
+			System.out.println(xx[i]);
+		*/
+	}
+}
