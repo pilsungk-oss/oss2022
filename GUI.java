8fb037b244451a4cb9d938602090c1a90e0e5a9f
pilsungk-oss <pilsungk+oss@gmail.com>
2022-04-18 10:32:34 +0900

    x is 3 in gui branch

--- GUI.java
+++ GUI.java
@@ -3,5 +3,8 @@ public class GUI {
     public static void main(String args[]) {
         System.out.println("GUI");
         System.out.println("2 in gui branch");
+		System.out.println("3 in gui branch");
+		
+		int x = 5;
     }
 }
